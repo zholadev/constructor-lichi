5c305617e3acb1c5559ddfa437c7a2ad50e7c4a6
zholadev <zholadev.io@gmail.com>
2024-10-20 17:53:27 +0500

    release: test

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,41 +2,28 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server"; // Импорт типов Next.js
 
 export function middleware(req: NextRequest): NextResponse {
-	// Проверяем окружение на production
-	if (process.env.NODE_ENV === "production") {
-		// Извлекаем заголовок авторизации
-		const auth = req.headers.get("authorization");
+	const auth = req.headers.get("authorization");
 
-		// Если заголовок авторизации присутствует
-		if (auth) {
-			// Декодируем и разбиваем строку на логин и пароль
-			const [user, pass] = Buffer.from(auth.split(" ")[1], "base64")
-				.toString()
-				.split(":");
+	if (auth) {
+		const [user, pass] = Buffer.from(auth.split(" ")[1], "base64")
+			.toString()
+			.split(":");
 
-			// Проверяем соответствие логина и пароля значениям переменных окружения
-			if (
-				user === process.env.AUTH_USER &&
-				pass === process.env.AUTH_PASS
-			) {
-				return NextResponse.next(); // Если логин и пароль совпадают, продолжаем выполнение
-			}
+		if (user === "xyzDev" && pass === "xyzDev") {
+			return NextResponse.next();
 		}
-
-		// Если авторизация не прошла, возвращаем ответ с кодом 401 и заголовком WWW-Authenticate
-		return new NextResponse("Auth Required", {
-			status: 401,
-			headers: {
-				"WWW-Authenticate": "Basic realm='Secure Area'",
-			},
-		});
 	}
 
-	// Если окружение не production или авторизация успешна
+	return new NextResponse("Auth Required", {
+		status: 401,
+		headers: {
+			"WWW-Authenticate": "Basic realm='Secure Area'",
+		},
+	});
+
 	return NextResponse.next();
 }
 
-// Конфигурация middleware для исключения маршрутов
 export const config = {
-	matcher: "/((?!api|_next/static|favicon.ico).*)", // Исключаем /api, /_next/static и favicon.ico
+	matcher: "/((?!api|_next/static|favicon.ico).*)",
 };

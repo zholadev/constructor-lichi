74941fbaac9c746974853ee6ca4728adca7fab32
zholadev <zholadev.io@gmail.com>
2024-10-21 15:50:11 +0500

    update: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest): NextResponse | any {
+export function middleware(req: NextRequest): NextResponse {
 	const auth = req.headers.get("authorization");
 
 	if (auth) {
